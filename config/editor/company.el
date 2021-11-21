e95799945efdbc7604c2cc980bc736dbe241d3c8
Sergey Mishin <sergei.a.mishin@gmail.com>
2021-11-21 07:42:21 +0300

    Extend company configuration

--- config/editor/company.el
+++ config/editor/company.el
@@ -1,2 +1,17 @@
 (use-package company
-  :ensure t)
+  :ensure t
+  :custom
+  (company-idle-delay 0.5)
+  (company-selection-wrap-around t)
+  :bind (:map company-active-map
+	      ("C-n". company-select-next)
+	      ("C-p". company-select-previous)
+	      ("M-<". company-select-first)
+	      ("M->". company-select-last)))
+
+(use-package yasnippet
+  :ensure
+  :config
+  (yas-reload-all)
+  (add-hook 'prog-mode-hook 'yas-minor-mode)
+  (add-hook 'text-mode-hook 'yas-minor-mode))
